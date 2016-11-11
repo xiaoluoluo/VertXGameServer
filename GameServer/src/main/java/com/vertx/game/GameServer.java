76a4bc2323c615d9d850d8301bdd344cc7ee3c22
luoxiaosong <784478582@qq.com>
2016-11-11 22:26:51 +0800

    创建maven工程

---
+++ GameServer/src/main/java/com/vertx/game/GameServer.java
@@ -0,0 +1,13 @@
+package com.vertx.game;
+
+/**
+ * Hello world!
+ *
+ */
+public class GameServer 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
